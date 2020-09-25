bfa6a211edf37a3aaf79bfc87162b475cfa2b18c
unknown <fcomanquehual@gmail.com>
2020-09-24 23:32:04 -0300

    actualizado el 24 de septiembre del 2020

--- 1_instalacion.R
+++ 1_instalacion.R
@@ -24,6 +24,7 @@ library(devtools)
 # install_github(c('SantanderMetGroup/climate4R.UDG@devel', 'SantanderMetGroup/loadeR'))
 # install_github('SantanderMetGroup/loadeR.2nc')
 # install_github("SantanderMetGroup/loadeR.java", force = TRUE)
+install_github('SantanderMetGroup/climate4R.datasets')
 install_github('SantanderMetGroup/climate4R.climdex')
 install_github('SantanderMetGroup/VALUE')
 install_github('SantanderMetGroup/climate4R.value')
@@ -39,6 +40,13 @@ install_github(c("SantanderMetGroup/loadeR.java",
 install_github("SantanderMetGroup/visualizeR")
 
 install_github("SantanderMetGroup/downscaleR")
+
+# herramientas extra:
+
+devtools::install_github("SantanderMetGroup/drought4R@v0.1.0",
+                         "SantanderMetGroup/convertR@v0.1.2") # convert, para convertir Kelvin a Celcius
+
+# fuente: http://www.meteo.unican.es/work/climate4r/drought4R/drought4R_notebook.html
 
 # library(installr)
 # uninstall.packages('fields')
